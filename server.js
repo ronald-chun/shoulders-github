30d0426f2b7874fccbcd88298e97f6f93f6cfd85
Ronald Wong <ronaldchunyin@gmail.com>
2018-07-21 21:43:58 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require('./app');
+const normalizePort = require('normalize-port');
+const port = normalizePort(process.env.PORT || '3000');
+
+app.set('port', port);
+
+http.createServer(app).listen(port, function() {
+	console.log('Server start running at port ' + port);
+});
+
+
